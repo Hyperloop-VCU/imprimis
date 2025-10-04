db45b5c8db8b08f46d4018fb00e52515b8328912
Mehak Chopra <chopram@vcu.edu>
2025-10-04 13:37:34 -0400

    Renamed package to imprimis_hardware_platform

--- imprimis_ws/build/imprimis/CMakeFiles/imprimis_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/imprimis_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/imprimis_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
