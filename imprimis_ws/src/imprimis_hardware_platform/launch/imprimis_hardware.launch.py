61db0caccfc977b49dfc1787606bac1c2a127acd
Ray Neralla <rayneralla@icloud.com>
2025-10-21 11:27:25 -0400

    Added gazebo simulation and modified hardware launch file

--- imprimis_ws/src/imprimis_hardware_platform/launch/imprimis_hardware.launch.py
+++ imprimis_ws/src/imprimis_hardware_platform/launch/imprimis_hardware.launch.py
@@ -1,34 +1,20 @@
-# Copyright 2020 ros2_control Development Team
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from launch import LaunchDescription
 from launch import LaunchContext
 from launch.substitutions import PythonExpression
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler, LogInfo
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, LogInfo, IncludeLaunchDescription, GroupAction
 from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
-
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from os import path as os_path
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
 
-    LogInfo(msg="Starting up imprimis hardware...")
-
-    # Declare arguments
+    # Declare and initialize arguments
     declared_arguments = []
     declared_arguments.append(
         DeclareLaunchArgument(
@@ -45,10 +31,18 @@ def generate_launch_description():
             description="Choose between real hardware, completely faked hardware, or gazebo-simulated hardware.",
         )
     )
-
-    # Initialize Arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "control_type",
+            default_value="other",
+            choices=("keyboard", "controller", "other"),
+            description="How imprimis will actually be controlled. Choice between keyboard, joystick controller, or other. Keyboard input is not implemented yet.",
+        )
+    )
     gui = LaunchConfiguration("gui")
     hardware_type = LaunchConfiguration("hardware_type")
+    control_type = LaunchConfiguration("control_type")
+
 
     # Get URDF via xacro
     robot_description_content = Command(
@@ -65,6 +59,8 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
+
+    # controller manager, if we are not using simulated hardware
     robot_controllers = PathJoinSubstitution(
         [
             FindPackageShare("imprimis_hardware_platform"),
@@ -72,25 +68,30 @@ def generate_launch_description():
             "diffbot_controllers.yaml",
         ]
     )
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("imprimis_description"), "rviz", "diffbot.rviz"]
-    )
-
-    control_node = Node(
+    controller_manager_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[robot_controllers],
         output="both",
         remappings=[
             ("~/robot_description", "/robot_description"),
-            ("/diffbot_base_controller/cmd_vel", "/cmd_vel"),
         ],
+        condition=IfCondition(PythonExpression(["'", hardware_type, "' != 'simulated'"]))
     )
+
+
+    # robot state publisher
     robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
         parameters=[robot_description],
+    )
+
+
+    # rviz
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("imprimis_description"), "rviz", "diffbot.rviz"]
     )
     rviz_node = Node(
         package="rviz2",
@@ -101,26 +102,89 @@ def generate_launch_description():
         condition=IfCondition(gui),
     )
 
+
+    # joint state broadcaster spawner, if we aren't using simulated hardware
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        condition=IfCondition(PythonExpression(["'", hardware_type, "' != 'simulated'"]))
     )
 
+
+    # Imprimis diff drive controller spawner
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diffbot_base_controller", "--controller-manager", "/controller_manager"],
     )
 
-    # Delay rviz start after `joint_state_broadcaster`
+
+    # Gazebo, if we're using simulated hardware
+    gazebo_launch_include = IncludeLaunchDescription(
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gazebo.launch.py'
+            ]),
+            condition=IfCondition(PythonExpression(["'", hardware_type, "' == 'simulated'"]))
+        )
+    
+
+    # Spawn imprimis into the gazebo simulation, if we're using simulated hardware
+    spawn_imprimis_gazebo = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=['-topic', 'robot_description', '-entity', 'imprimis'],
+        output="screen",
+        condition=IfCondition(PythonExpression(["'", hardware_type, "' == 'simulated'"]))
+    )
+    
+
+    # Controller input, if we specified to use it
+    controller_input_launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('teleop_twist_joy'),
+                'launch',
+                'teleop-launch.py'
+            ])
+        ]),
+        launch_arguments={
+                'joy_config': 'xbox',
+                "publish_stamped_twist": 'true',
+                'frame': 'base_link',
+                'joy_vel': 'diffbot_base_controller/cmd_vel'
+                }.items(),
+        condition=IfCondition(PythonExpression(["'", control_type, "' == 'controller'"]))
+        )
+    
+    
+
+    things_to_launch = [
+        controller_manager_node,         # not in sim
+        robot_state_pub_node,
+        robot_controller_spawner,
+        rviz_node,
+        joint_state_broadcaster_spawner, # not in sim
+        gazebo_launch_include,           # sim only
+        spawn_imprimis_gazebo,           # sim only
+        controller_input_launch_include, # if we need it
+    ]
+
+    return LaunchDescription(declared_arguments + things_to_launch)
+
+
+
+"""
+Old code for delaying node startup
+# Delay rviz start after `joint_state_broadcaster`
     delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=joint_state_broadcaster_spawner,
             on_exit=[rviz_node],
         )
     )
-
     # Delay start of joint_state_broadcaster after `robot_controller`
     # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
     delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
@@ -129,14 +193,4 @@ def generate_launch_description():
             on_exit=[joint_state_broadcaster_spawner],
         )
     )
-
-    nodes = [
-        control_node,
-        robot_state_pub_node,
-        robot_controller_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
-        delay_joint_state_broadcaster_after_robot_controller_spawner,
-
-    ]
-
-    return LaunchDescription(declared_arguments + nodes)
+"""
