fcf895dbc7e3f5d460013b4af8d59727103821e4
Ray Neralla <rayneralla@icloud.com>
2025-12-18 16:01:59 -0500

    Refined board A logic for mode switching

--- EspFirmware/boardA/src/boardA.cpp
+++ EspFirmware/boardA/src/boardA.cpp
@@ -18,14 +18,12 @@ bool is_connected = false;
 FlyskyIBUS IBUS(Serial2, RC_IBUS_RX, 17);
 std::atomic<float> leftAngvel{0.0};
 std::atomic<float> rightAngvel{0.0};
-std::atomic<unsigned long> time_of_last_data_receive{millis()};
 
 
 // Receive data callback: runs whenever B sends wheel angvels to A
 void receiveDataCB(const uint8_t * mac, const uint8_t *incomingData, int len) 
 {
-  time_of_last_data_receive = millis();
-
+  is_connected = true; // assume we will always be able to send data if we can receive it
   // copy recieved bytes into the struct so we can access/modify the data
   struct BtoAPacket received_data;
   if (len != sizeof(BtoAPacket)) return;
@@ -48,63 +46,49 @@ void sendDataCB(const uint8_t *mac_addr, esp_now_send_status_t status)
 }
 
 
-// Updates the data packet for autonomous mode and prints the latest wheel data to serial.
-// Does nothing if there is no command, or the command is invalid.
-void handle_ROS_command(struct AtoBPacket& data_to_send) 
+// Updates the data to send based on the command sent and prints data to serial.
+// Does nothing if there is no command, or the command is invalid. Does not print to serial if we've lost connection with board B.
+// Returns true if a valid command was received, false otherwise.
+bool handle_ROS_command(struct AtoBPacket& data_to_send) 
 {
-  if (!Serial.available()) return;
+  if (!Serial.available()) return false;
+
   char chr = Serial.read();
   switch(chr) {
-
     case RESET_ENCODERS:
       data_to_send.reset = true;
       break;
-
-    case ANGVEL_SETPOINT: {
+    case ANGVEL_SETPOINT:
       serialReadFloat(data_to_send.setLeftAngvel);
       serialReadFloat(data_to_send.setRightAngvel);
       break;
-    }
-
-    case SET_PID: {
+    case SET_PID:
       serialReadFloat(data_to_send.newKp);
       serialReadFloat(data_to_send.newKi);
       serialReadFloat(data_to_send.newKd);
       serialReadInt(data_to_send.gainChange);
       break;
-    }
     default:
-      return;
+      return false;
   }
 
-  // print latest wheel data and mode if we still have connection to board B
-  struct BtoAPacket received_data;
-  if (millis() - time_of_last_data_receive < 500) {
-    float left = leftAngvel;
-    float right = rightAngvel;
-    Serial.printf("%+06.2f %+06.2f %d\n", left, right, data_to_send.openLoop);
-  }
-  // lost connection
-  else {
-    leftAngvel = 0.0;
-    rightAngvel = 0.0;
-  }
+  if (!is_connected) return true;
+  struct BtoAPacket received_data{};
+  float left = leftAngvel;
+  float right = rightAngvel;
+  Serial.printf("%+06.2f %+06.2f %d\n", left, right, data_to_send.openLoop);
+  return true;
 }
 
 // Updates the data packet for manual mode.
 void handle_manual_commands(struct AtoBPacket& data_to_send)
 {
-  // get left and right efforts
   float x_normalized = (IBUS.getChannel(0) - 1500.0) / 500.0;
   float y_normalized = (IBUS.getChannel(1) - 1500.0) / 500.0;
   data_to_send.setLeftAngvel = y_normalized;
   data_to_send.setRightAngvel = y_normalized;
   data_to_send.setLeftAngvel += x_normalized;
   data_to_send.setRightAngvel -= x_normalized;
-
-  // prevent ROS from doing anything
-  data_to_send.gainChange = 0;
-  data_to_send.reset = false;
 }
 
 
@@ -145,19 +129,21 @@ void loop()
     digitalWrite(YELLOW_LIGHT, (status_YellowLightOn ? HIGH : LOW));
     status_YellowLastSwitched = millis();
   }
-  
-  // handle motor control, ROS, and send data to board B
-  struct AtoBPacket data_to_send{};
-  data_to_send.openLoop = !autonomous;
-  handle_ROS_command(data_to_send);
-  if (data_to_send.openLoop) {
-    handle_manual_commands(data_to_send);
-    delay(10);
-  }
-  esp_now_send(B_MAC, (uint8_t*)&data_to_send, sizeof(data_to_send));
-
-  
 
   // handle green light
   digitalWrite(GREEN_LIGHT, (is_connected ? HIGH : LOW));
+
+  // forward PC->A data to B if needed, print B->A data to serial if needed
+  struct AtoBPacket data_to_send{};
+  data_to_send.openLoop = !autonomous;
+  bool command_received = handle_ROS_command(data_to_send);
+  if (autonomous && command_received) {
+    esp_now_send(B_MAC, (uint8_t*)&data_to_send, sizeof(AtoBPacket));
+  }
+  else if (!autonomous) {
+    handle_manual_commands(data_to_send);
+    esp_now_send(B_MAC, (uint8_t*)&data_to_send, sizeof(AtoBPacket));
+  }
+
+  
 }
