218a62aa48366d84e9a00f7e0326c0dc2bd6b48a
Ray Neralla <rayneralla@icloud.com>
2026-02-13 18:34:29 -0500

    Adjusted dependencies and gazebo fixer script for jazzy

--- imprimis_ws/fix_gazebo.bash
+++ imprimis_ws/fix_gazebo.bash
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Get current script directory
+set -euo pipefail
+SCRIPT_DIR="$(cd -- "$(dirname -- "$(readlink -f -- "$0")")" && pwd)"
+
 # Allow multicast
 sudo ufw allow in proto udp to 224.0.0.0/4
 sudo ufw allow in proto udp from 224.0.0.0/4
 
-# Add imprimis meshes to gazebo's path
-source ./install/setup.bash
-LINE='export IGN_GAZEBO_RESOURCE_PATH=$IGN_GAZEBO_RESOURCE_PATH:$(ros2 pkg prefix imprimis_description)/share'
-grep -qxF "$LINE" "$HOME/.bashrc" || echo "$LINE" >>  "$HOME/.bashrc"
+# Source imprimis automatically and add imprimis meshes to gazebo's path
+LINE1="source $SCRIPT_DIR/install/setup.bash"
+LINE2='export GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH:+$GZ_SIM_RESOURCE_PATH:}$(ros2 pkg prefix imprimis_description)/share'
+
+# Add those two commands to bashrc, if they aren't already in there
+grep -qxF "$LINE1" "$HOME/.bashrc" || echo "$LINE1" >> "$HOME/.bashrc"
+grep -qxF "$LINE2" "$HOME/.bashrc" || echo "$LINE2" >> "$HOME/.bashrc"
+
+# Run those two commands
 source ~/.bashrc
+
 
 
 
